6a9c32f5cd7101f3e12c8a3f6d37726675fdd9a0
jfanzago <jfanzago@gmail.com>
2016-03-03 14:00:16 -0500

    added styles

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,5 +1,8 @@
 class Home < ActiveRecord::Base
 
+	mount_uploaders :avatars, AvatarUploader
+	
+
 	def self.search(search)		
     	where("city ILIKE ? OR cast(zip as text) ILIKE ? OR state ILIKE ?", search, search, search)
 	end
